91b6848204ba1c6fe8efeb255d285e21a78348f9
Braden Steffaniak <BradenSteffaniak@gmail.com>
2026-01-18 09:15:45 -0500

    Do not turn of nixos-desktop if lid closed

--- hosts/nixos-desktop/default.nix
+++ hosts/nixos-desktop/default.nix
@@ -126,6 +126,12 @@
     ];
   };
 
+  services.logind.settings.Login = {
+    HandleLidSwitch = "ignore";
+    HandleLidSwitchExternalPower = "ignore";
+    HandleLidSwitchDocked = "ignore";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
