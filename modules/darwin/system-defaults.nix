fbe622ddfb1c2ac651eb83fe04a0a8aac014976c
Braden Steffaniak <BradenSteffaniak@gmail.com>
2026-02-05 07:30:00 -0500

    Hide desktop icons on mac by default

--- modules/darwin/system-defaults.nix
+++ modules/darwin/system-defaults.nix
@@ -10,11 +10,19 @@ with lib;
 {
   options.myConfig.darwin.systemDefaults = {
     enable = mkEnableOption "macOS system defaults";
+
+    hideDesktopIcons = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Hide all icons on the desktop (files remain in ~/Desktop but are not shown)";
+    };
   };
 
   config = mkIf config.myConfig.darwin.systemDefaults.enable {
     system.defaults = {
       dock.autohide = true;
+
+      finder.CreateDesktop = !config.myConfig.darwin.systemDefaults.hideDesktopIcons;
     };
   };
 }
