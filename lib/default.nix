1848af71bb292616b1fe9e3410890aafe51fa29e
Braden Steffaniak <BradenSteffaniak@gmail.com>
2026-02-25 09:28:08 -0500

    Remove unused helpers

--- lib/default.nix
+++ lib/default.nix
@@ -1,15 +1,6 @@
 { lib, ... }:
 
 {
-  # Helper to check if we're on Darwin
-  isDarwin = system: lib.hasSuffix "-darwin" system;
-
-  # Helper to check if we're on Linux
-  isLinux = system: lib.hasSuffix "-linux" system;
-
-  # Helper to conditionally include packages based on platform
-  optionalPkgs = condition: pkgs: if condition then pkgs else [ ];
-
   # Helper for enable options with a custom default value
   # Usage: mkEnableOption' cfg.enableAll "Description of the feature"
   mkEnableOption' =
